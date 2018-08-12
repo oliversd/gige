299458b5e92f0fa2684722f495ee3360eea6c768
Olivers De Abreu <oliversd@gmail.com>
2018-08-12 17:08:33 -0300

    Added navbar and new color palette

--- src/themes/theme1/palete.js
+++ src/themes/theme1/palete.js
@@ -1,19 +1,19 @@
 const palete = {
   primary: {
-    light: '#90b3c8',
-    main: '#216790',
-    dark: '#18547d',
-    contrastText: '#fff'
+    light: "#C5CAE9",
+    main: "#3F51B5",
+    dark: "#303F9F",
+    contrastText: "#fff"
   },
   secondary: {
-    light: '#ffc87e',
-    main: '#ffb147',
-    dark: '#ff982f',
-    contrastText: '#144a73'
+    light: "#8BC34A",
+    main: "#CDDC39",
+    dark: "#689F38",
+    contrastText: "#757575"
   },
   text: {
-    primary: '#fff',
-    secondary: '#144a73'
+    primary: "#212121",
+    secondary: "#757575"
   }
 };
 
