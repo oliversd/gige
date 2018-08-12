2761e38519d31435e855546edcdd5df652058b24
Olivers De Abreu <oliversd@gmail.com>
2018-08-12 15:51:41 -0300

    Remove language switcher import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./screens/Home";
-import LanguageSwitcher from "./components/LanguageSwitcher";
 import NotFound from "./screens/NotFound";
 
 const App = () => (
