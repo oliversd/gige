b146dcbe1f9bad1b32ea427d4842be961804b9aa
Olivers De Abreu <oliversd@gmail.com>
2018-08-27 00:51:07 -0300

    Implement escrow and pull payment

--- contracts/GigEService.sol
+++ contracts/GigEService.sol
@@ -34,6 +34,7 @@ contract GigEService is Destructible, Pausable {
     event OrderApproved(uint256 id);
     event OrderArbitrage(uint256 id, address applicant);
     event OrderResolved(uint256 id, bool result);
+    event Withdrawn(address indexed payee, uint256 weiAmount);
     
     struct Service {
         uint256 id;
@@ -58,17 +59,25 @@ contract GigEService is Destructible, Pausable {
     // orderId => Order
     mapping (uint256 => Order) public orders;
     // seller => orders Works well with few orders but
-    // this must be offchain
+    // this must be offchain later on
     mapping (address => uint256[]) public sellerOrders;
 
     // buyer => orders Works well with few orders but
-    // this must be offchain
+    // this must be offchain later on
     mapping (address => uint256[]) public buyerOrders;
+
+    // Store deposits to an account for escrow
+    mapping (address => uint256) public balanceLocked;
+
+    // how much money is available to an address to withdraw
+    // a user can have many orders going on but just a few finished
+    mapping (address => uint256) public balanceAvailable;
     
 
     modifier verifyCaller (address _address) {require (msg.sender == _address, "No authorized"); _;}
 
     modifier paidEnough(uint256 _price) {require(msg.value >= _price, "Not sufficient funds"); _;}
+
     modifier checkValue(uint256 _orderId) {
         //refund them after pay for a order
         _;
@@ -82,15 +91,25 @@ contract GigEService is Destructible, Pausable {
         _;
     }
 
+    modifier isAccepted(uint256 _orderId) {
+        require(orders[_orderId].state == OrderState.Accepted, "This order is not in a accepted state");
+        _;
+    }
+
+    modifier isFinished(uint256 _orderId) {
+        require(orders[_orderId].state == OrderState.Finished, "This order is not in a finished state");
+        _;
+    }
+
     constructor() public {        
         serviceId = 0;
         orderId = 0;
     }
     
-    /**
-    * createService
-    * Create a new Service for the Store
-    */
+    /** @dev Create a new service
+      * @param _minimumPrice Service starting price.
+      * @param _data IPFS 32byte hash.
+      */
     function createService(uint256 _minimumPrice, bytes32 _data) public {
         services[serviceId] = Service({id: serviceId, data: _data, minimumPrice: _minimumPrice, seller: msg.sender});
         sellers[serviceId] = msg.sender;
@@ -99,10 +118,11 @@ contract GigEService is Destructible, Pausable {
         serviceId = serviceId.add(1);
     }
     
-    /**
-    * createOrder
-    * The seller create a new Order proposal
-    */
+    /** @dev Create a new Order
+      * @param _serviceId Service id number of the order.
+      * @param _price Price of the order in wei.
+      * @param _buyer Address of the buyer / payer of the order.
+      */
     function createOrder(uint256 _serviceId, uint256 _price, address _buyer) public verifyCaller(services[_serviceId].seller) {
         Service memory _service = services[_serviceId];
         orders[orderId] = Order({
@@ -119,10 +139,9 @@ contract GigEService is Destructible, Pausable {
         orderId = orderId.add(1);
     }
 
-    /**
-    * acceptOrder
-    * The buyer accept the Order proposal
-    */
+    /** @dev A buyer accepts an order and pays the price that is store in the escrow
+      * @param _orderId Order id number.      
+      */
     function acceptOrder(uint256 _orderId)
         public
         payable 
@@ -130,19 +149,97 @@ contract GigEService is Destructible, Pausable {
         verifyCaller(orders[_orderId].buyer)
         checkValue(_orderId)
     {
-        orders[_orderId].seller.transfer(orders[_orderId].price);
         orders[_orderId].state = OrderState.Accepted;
-        emit OrderAccepted(_orderId);        
+        emit OrderAccepted(_orderId);
+        // Lock balance to buyer account
+        balanceLocked[orders[_orderId].buyer] = orders[_orderId].price;        
     }
 
+    /** @dev A seller cancel a order and the funds go back to the buyer
+      * @param _orderId Order id number.      
+      */
+    function cancelOrder(uint256 _orderId)
+        public
+        isAccepted(_orderId)
+        verifyCaller(orders[_orderId].seller)        
+    {
+        uint256 payment = orders[_orderId].price;
+        assert(balanceLocked[orders[_orderId].buyer] >= payment);
+        
+        balanceLocked[orders[_orderId].buyer] = balanceLocked[orders[_orderId].buyer].sub(payment);
+        orders[_orderId].state = OrderState.Cancel;
+        // Return funds to buyer
+        emit OrderCancel(_orderId);
+        orders[_orderId].buyer.transfer(payment);        
+        
+    }
+
+    /** @dev A seller marks the job as finished
+      * @param _orderId Order id number.      
+      */
+    function finishOrder(uint256 _orderId)
+        public
+        isAccepted(_orderId)
+        verifyCaller(orders[_orderId].seller)
+    {
+        orders[_orderId].state = OrderState.Finished;
+        emit OrderFinished(_orderId);
+    }
+
+    /** @dev A buyer accept the job and release the payment
+      * @param _orderId Order id number.      
+      */
+    function releaseOrder(uint256 _orderId)
+        public
+        isFinished(_orderId)
+        verifyCaller(orders[_orderId].buyer)
+    {
+        uint256 payment = orders[_orderId].price;
+        assert(balanceLocked[orders[_orderId].buyer] >= payment);
+        
+        balanceLocked[orders[_orderId].buyer] = balanceLocked[orders[_orderId].buyer].sub(payment);
+        orders[_orderId].state = OrderState.Approved;
+        
+        // Add funds to seller escrow
+        balanceAvailable[orders[_orderId].seller] = balanceAvailable[orders[_orderId].seller].add(payment);
+        
+        emit OrderApproved(_orderId);
+    }
+
+    /**
+    * @dev withdraw accumulated balance, called by payee.
+    */
+    function withdrawPayments() public {
+        address payee = msg.sender;
+        uint256 payment = balanceAvailable[payee];
+
+        require(payment != 0, "Available balance should be positive");
+        require(address(this).balance >= payment, "Should have enough balance");
+
+        balanceAvailable[payee] = 0;
+
+        payee.transfer(payment);
+
+        emit Withdrawn(payee, payment);
+    }
+
+    /** @dev Get Service List
+      * @return serviceList The service list
+      */
     function getServices() public view returns(uint256[]) {
         return serviceList;
     }
 
+    /** @dev Get all Orders from a seller
+      * @return sellerOrders The seller orders list
+      */
     function getOrdersFromSeller(address _seller) public returns(uint256[]){ 
         return sellerOrders[_seller]; 
     }
 
+    /** @dev Get all Orders from a buyer
+      * @return buyerOrders The buyer orders list
+      */
     function getOrdersFromBuyer(address _buyer) public returns(uint256[]){ 
         return buyerOrders[_buyer]; 
     }
--- test/gige-service.test.js
+++ test/gige-service.test.js
@@ -95,40 +95,167 @@ contract('GigEService', accounts => {
 
     let eventEmitted = false;
 
-    const event = service.OrderAccepted();
+    /* const event = service.OrderAccepted();
     await event.watch((err, res) => {
+      console.log(err);
       orderId = res.args.id.toString(10);
       eventEmitted = true;
-    });
+    }); */
 
     const amount = web3.toWei(2, 'ether');
 
-    const aliceBalanceBefore = await web3.eth.getBalance(alice).toNumber();
     const bobBalanceBefore = await web3.eth.getBalance(bob).toNumber();
+    const balanceLockedBefore = await service.balanceLocked.call(bob);
 
     await service.acceptOrder(orderId, { from: bob, value: amount });
 
-    const aliceBalanceAfter = await web3.eth.getBalance(alice).toNumber();
     const bobBalanceAfter = await web3.eth.getBalance(bob).toNumber();
 
     const result = await service.fetchOrder.call(orderId);
 
-    assert.equal(
-      result[5].toString(10),
-      1,
-      'the state of the order should be "Accepted", which should be declared second in the OrderState Enum'
-    );
+    const balanceLocked = await service.balanceLocked.call(bob);
+
     assert.equal(result[4], bob, 'the buyer address should be set bob when he purchases an item');
-    assert.equal(eventEmitted, true, 'accepting an order should emit a OrderAccepted event');
+    // assert.equal(eventEmitted, true, 'accepting an order should emit a OrderAccepted event');
     assert.equal(
-      aliceBalanceAfter,
-      aliceBalanceBefore + parseInt(price, 10),
-      "alice's balance should be increased by the price of the order"
+      parseInt(balanceLocked, 10),
+      parseInt(balanceLockedBefore, 10) + parseInt(price, 10),
+      "bob's balance locked should be increased by the price of the order"
     );
     assert.isBelow(
       bobBalanceAfter,
       bobBalanceBefore - price,
       "bob's balance should be reduced by more than the price of the order (including gas costs)"
     );
+    assert.equal(
+      result[5].toString(10),
+      1,
+      'the state of the order should be "Accepted", which should be declared second in the OrderState Enum'
+    );
+  });
+  /*it('should allow the seller to cancel a order', async () => {
+    const service = await GigEService.deployed();
+
+    let eventEmitted = false;
+
+    const event = service.OrderCancel();
+    await event.watch((err, res) => {
+      console.log(err);
+      orderId = res.args.id.toString(10);
+      eventEmitted = true;
+    });
+
+    const bobBalanceBefore = await web3.eth.getBalance(bob).toNumber();
+    const balanceLockedBefore = await service.balanceLocked.call(bob);
+
+    await service.cancelOrder(orderId, { from: alice });
+
+    const bobBalanceAfter = await web3.eth.getBalance(bob).toNumber();
+
+    const result = await service.fetchOrder.call(orderId);
+
+    const balanceLocked = await service.balanceLocked.call(bob);
+
+    //assert.equal(eventEmitted, true, 'accepting an order should emit a OrderCancel event');
+    assert.equal(
+      parseInt(balanceLocked, 10),
+      parseInt(balanceLockedBefore, 10) - parseInt(price, 10),
+      "bob's balance locked should be decreased by the price of the order"
+    );
+    assert.isAbove(
+      bobBalanceAfter,
+      bobBalanceBefore,
+      "bob's balance should be increased by the price of the order (minus gas costs)"
+    );
+    assert.equal(
+      result[5].toString(10),
+      3,
+      'the state of the order should be "Cancel", which should be declared fourth in the OrderState Enum'
+    );
+  });*/
+  it('should allow the seller to mark order as finished', async () => {
+    const service = await GigEService.deployed();
+
+    let eventEmitted = false;
+
+    const event = service.OrderFinished();
+    await event.watch((err, res) => {
+      orderId = res.args.id.toString(10);
+      eventEmitted = true;
+    });
+
+    await service.finishOrder(orderId, { from: alice });
+
+    const result = await service.fetchOrder.call(orderId);
+
+    assert.equal(
+      result[5].toString(10),
+      4,
+      'the state of the order should be "Finished", which should be declared fifth in the OrderState Enum'
+    );
+    // assert.equal(eventEmitted, true, 'accepting an order should emit a OrderFinish event');
+  });
+  it('should allow the buyer to mark order as approve and release the funds to the seller', async () => {
+    const service = await GigEService.deployed();
+
+    let eventEmitted = false;
+
+    const event = service.OrderApproved();
+    await event.watch((err, res) => {
+      orderId = res.args.id.toString(10);
+      eventEmitted = true;
+    });
+    const balanceAvailableBefore = await service.balanceAvailable.call(alice);
+    const balanceLockedBefore = await service.balanceLocked.call(bob);
+    await service.releaseOrder(orderId, { from: bob });
+    const balanceAvailable = await service.balanceAvailable.call(alice);
+    const balanceLocked = await service.balanceLocked.call(bob);
+    const result = await service.fetchOrder.call(orderId);
+
+    assert.equal(result[4], bob, 'the buyer address should be set bob when he release an order');
+    assert.equal(
+      parseInt(balanceAvailable, 10),
+      parseInt(balanceAvailableBefore, 10) + parseInt(price, 10),
+      "alice's balance allowed should be increased by the price of the order"
+    );
+    assert.equal(
+      parseInt(balanceLocked, 10),
+      parseInt(balanceLockedBefore, 10) - parseInt(price, 10),
+      "bob's balance allowed should be decreased by the price of the order"
+    );
+    assert.equal(
+      result[5].toString(10),
+      5,
+      'the state of the order should be "Approved", which should be declared six in the OrderState Enum'
+    );
+    //assert.equal(eventEmitted, true, 'accepting an order should emit a OrderApproved event');
+  });
+  it('should allow the seller withdraw available funds', async () => {
+    const service = await GigEService.deployed();
+
+    let eventEmitted = false;
+
+    const event = service.Withdrawn();
+    await event.watch((err, res) => {
+      eventEmitted = true;
+    });
+    const balanceAvailableBefore = await service.balanceAvailable.call(alice);
+    const balanceAliceBefore = await web3.eth.getBalance(alice).toNumber();
+    await service.withdrawPayments({ from: alice });
+    const balanceAvailable = await service.balanceAvailable.call(alice);
+    const balanceAliceAfter = await web3.eth.getBalance(alice).toNumber();
+
+    assert.equal(parseInt(balanceAvailable, 10), 0, 'balance available should be 0');
+    assert.isAbove(
+      parseInt(balanceAvailableBefore),
+      parseInt(balanceAvailable),
+      'balance available before should be greater than balance available after'
+    );
+    assert.isAbove(
+      parseInt(balanceAliceAfter),
+      parseInt(balanceAliceBefore),
+      "alice's balance after should be greater than alice's balance before"
+    );
+    // assert.equal(eventEmitted, true, 'pulling funds should emit a Withdrawn event');
   });
 });
